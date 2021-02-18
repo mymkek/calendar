e49a77e14930903a52c9c6b10ec078dd8226abf1
mymkek <mymkek@gmail.com>
2021-02-18 11:49:56 +0300

    change port

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const fs = require("fs");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const jsonParser = express.json();
 
 app.use(express.static(__dirname + "/public"));
